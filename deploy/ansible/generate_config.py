a22f955a30b78a07f660a1fd9b262681564b9a24
dumitru <dimasavva17@gmail.com>
2017-11-16 11:43:57 +0300

    deploy iroha network on clean machines
    Signed-off-by: dumitru <dimasavva17@gmail.com>

---
+++ deploy/ansible/generate_config.py
@@ -0,0 +1,31 @@
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option('-p', '--postgres-password',
+                  help="postgress password")
+
+parser.add_option('-u', '--postgres-user',
+                  help="postgress user")
+
+
+options, args = parser.parse_args()
+
+
+print (options.postgres_password)
+
+with open("/data/config.sample", "w") as f:
+    s = """{{
+  "block_store_path" : "/tmp/block_store/",
+  "torii_port" : 50051,
+  "internal_port" : 10001,
+  "pg_opt" : "host=iroha_postgres port=5432 user={} password={}",
+  "redis_host" : "iroha_redis",
+  "redis_port" : 6379,
+  "max_proposal_size" : 10,
+  "proposal_delay" : 5000,
+  "vote_delay" : 5000,
+  "load_delay" : 5000
+}}""".format(options.postgres_user, options.postgres_password)
+
+    f.write(s)
